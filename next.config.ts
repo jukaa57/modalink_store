56fe5cb1a94d832678d53571f5a97dd38758e38a
jukaa57 <junioroliveira.1150@gmail.com>
2025-08-02 17:05:40 -0300

    style: logo, favicon and theme colors created

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [new URL("https://fakestoreapi.com/img/*"),]
+  },
+  env: {
+    API_URL: process.env.API_URL,
+  }
 };
 
 export default nextConfig;
